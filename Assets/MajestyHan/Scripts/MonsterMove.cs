68c76ff7c7c6a8b18b880d4d6e7168980bca786f
Dareuta <jk99333@gmail.com>
2025-05-13 15:55:51 +0900

    올리고싶은 기분이라서 올림
    두더지 좌우이동 구현중

--- Assets/MajestyHan/Scripts/MonsterMove.cs
+++ Assets/MajestyHan/Scripts/MonsterMove.cs
@@ -1,13 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterMove : MonoBehaviour
 {
-    public moveSpeed;
+    public float moveSpeed = 2f;
+    public float patrolDuration = 2f; // 왕복 주기 절반 (ex: 2초 동안 한 방향)
 
+    private Rigidbody2D rb;
+    private float timer = 0f;
+    private int direction = 1;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
+    void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
 
+        // 일정 시간 지나면 방향 반전
+        if (timer >= patrolDuration)
+        {
+            direction *= -1;
+            timer = 0f;
 
+            // 스프라이트 반전
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Sign(direction);
+            transform.localScale = scale;
+        }
+
+        rb.velocity = new Vector2(moveSpeed * direction, rb.velocity.y);
+    }
 }
