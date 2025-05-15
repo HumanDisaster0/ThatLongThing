58c92ee26cde61a5ad233613e9ae0fb534de360b
minkee009 <minkee5004@gmail.com>
2025-05-15 19:17:05 +0900

    플레이어 레드존 수정

--- Assets/Minki/Scripts/Player/PlayerRedzoneHit.cs
+++ Assets/Minki/Scripts/Player/PlayerRedzoneHit.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerRedzoneHit : MonoBehaviour
 {
+    public string RedZoneName = "RedZone";
+
+    PlayerController m_playerController;
+
+    private void Start()
+    {
+        m_playerController = GetComponent<PlayerController>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Redzone"))
+        if(collision.CompareTag(RedZoneName))
         {
             PlayerSpawnManager.instance.Respawn();
+            m_playerController.SetVelocity(Vector2.zero);
         }
     }
 }
