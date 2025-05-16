9c1485dba376d9e4e56b6ccc0c650fd09f60d549
Foodie_Pudding <sacar1920@gmail.com>
2025-05-16 15:41:43 +0900

    클리어 효과 추가
    ㅁㄴㅇㅁㄴㅇ

--- Assets/Sunken/Scripts/ClearZone/ClearZone.cs
+++ Assets/Sunken/Scripts/ClearZone/ClearZone.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class ClearZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float scaleSpeed = 1f;
+    [SerializeField] float moveSpeed = 1f;
+
+    GameObject target = null;
+    bool playAnim = false;
+
+
+    private void Update()
     {
-        
+        if(playAnim && target != null)
+        {
+            PlayerController pc = target.GetComponent<PlayerController>();
+            pc.SetVelocity(Vector2.zero);
+
+            if (pc.playerScale > 0f)
+                pc.playerScale = pc.playerScale - scaleSpeed * Time.deltaTime;
+            pc.ApplyScale();
+
+            Vector3 dir = transform.position - target.transform.position;
+            target.transform.position += dir * moveSpeed * Time.deltaTime;
+
+            if(Vector2.Distance(target.transform.position, transform.position) < 0.1f)
+            {
+                // 씬 체인지
+                Debug.Log("씬 체인지!");
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Player")
+        {
+            Debug.Log("플레이어 감지!");
+            target = collision.gameObject;
+            playAnim = true;
+        }
     }
 }
