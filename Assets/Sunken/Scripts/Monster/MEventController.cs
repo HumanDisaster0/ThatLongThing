eb380e3a55560acb9dd15a663e4eff6b1baacc7a
minkee009 <minkee5004@gmail.com>
2025-05-16 14:43:07 +0900

    Update MEventController.cs

--- Assets/Sunken/Scripts/Monster/MEventController.cs
+++ Assets/Sunken/Scripts/Monster/MEventController.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class MEventController : MonoBehaviour
 {
     [SerializeField] MDamageEvent mEvent;
+    PlayerController pc;
+
+    private void Start()
+    {
+        pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
 
     void Attack()
     {
         //TODO :: 플레이어 죽으면 쓰는 함수 집어넣기
         if (mEvent.isTouchingPlayer)
-            PlayerSpawnManager.instance.Respawn();
+            pc.AnyState(PlayerState.Die);
             //gameManager.SendMessage("Respawn");
     }
 }
