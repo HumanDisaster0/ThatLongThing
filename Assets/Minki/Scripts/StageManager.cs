b1c7ef80d0277531abba4edcdfb40f86cf9125c9
minkee009 <minkee5004@gmail.com>
2025-05-17 15:47:53 +0900

    스테이지2 완성

--- Assets/Minki/Scripts/StageManager.cs
+++ Assets/Minki/Scripts/StageManager.cs
@@ -48,7 +48,7 @@ public class StageManager : MonoBehaviour
         }
 
         //함정이 모두 꺼진 경우 랜덤 하나 켜기
-        if(result.Length - 1 == offTraps)
+        if(result.Length == offTraps)
         {
             result[Random.Range(0, result.Length)].SpecifiedSet(true);
         }
@@ -57,15 +57,20 @@ public class StageManager : MonoBehaviour
         switch (anomalyIdx)
         {
             //no.2 - 쥬라식 던전
-            //todo - 미완성, 티라노 추가해야함
             case 2:
-                GameObject.Find("PlatformManager").GetComponent<PlatformManager>().SetPlatformType(PlatformType.Alter);
-                break;
+                {
+                    GameObject.Find("PlatformManager").GetComponent<PlatformManager>().SetPlatformType(PlatformType.Alter);
+                    GameObject.Find("Anomaly").transform.Find("Trex").gameObject.SetActive(true);
+                    break;
+                }
+
 
             //no.3 - 복슬복슬한 행복
             case 3:
-                GameObject.Find("MonsterManager").GetComponent<MonsterManager>().SetType(MonsterType.Rabbit);
-                break;
+                {
+                    GameObject.Find("MonsterManager").GetComponent<MonsterManager>().SetType(MonsterType.Rabbit);
+                    break;
+                }
 
             //no.4 - 날아오르라 주작이여
             case 4:
@@ -103,84 +108,80 @@ public class StageManager : MonoBehaviour
 
             //no.7 - 나 홀로 던전
             case 7:
-                GameObject.Find("MonsterManager").GetComponent<MonsterManager>().SetType(MonsterType.None);
-                foreach (var setter in result)
                 {
-                    setter.SpecifiedSet(false);
+                    GameObject.Find("MonsterManager").GetComponent<MonsterManager>().SetType(MonsterType.None);
+                    foreach (var setter in result)
+                    {
+
+                        setter.SpecifiedSet(false);
+                    }
+                    break;
                 }
-                break;
+                
 
             //no.9 - 거인국 네티
-            //todo - 부수는거 연결, 플레이어 무적
+            //todo - 플레이어 무적
             case 9:
-                var pc = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
-                pc.playerScale = 4.0f;
-                pc.ApplyScale();
-                var destroyer = pc.AddComponent<Destroyer>();
-                destroyer.detectionRadius = 2.5f;
-                destroyer.destructibleTilemaps = new UnityEngine.Tilemaps.Tilemap[1];
-                destroyer.destructibleTilemaps[0] = GameObject.Find("Ground").GetComponent<Tilemap>();
-                destroyer.tilemapYMin = -2;
+                {
+                    var pc = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+                    pc.playerScale = 4.0f;
+                    pc.ApplyScale();
+                    var destroyer = pc.AddComponent<Destroyer>();
+                    destroyer.detectionRadius = 2.5f;
+                    destroyer.destructibleTilemaps = new UnityEngine.Tilemaps.Tilemap[1];
+                    destroyer.destructibleTilemaps[0] = GameObject.Find("Ground").GetComponent<Tilemap>();
+                    destroyer.tilemapYMin = -2;
 
-                var trapTrigger = pc.AddComponent<TrapTrigger>();
-                trapTrigger.OnResetTrigger = new UnityEngine.Events.UnityEvent();
-                trapTrigger.OnResetTrigger.AddListener(destroyer.RestoreDestroyedTiles);
+                    var trapTrigger = pc.AddComponent<TrapTrigger>();
+                    trapTrigger.OnResetTrigger = new UnityEngine.Events.UnityEvent();
+                    trapTrigger.OnResetTrigger.AddListener(destroyer.RestoreDestroyedTiles);
 
-                var flyingTile = Resources.Load<GameObject>("FlyingThings");
-                destroyer.flyingTilePrefab = flyingTile;
+                    var flyingTile = Resources.Load<GameObject>("FlyingThings");
+                    destroyer.flyingTilePrefab = flyingTile;
 
-                destroyer.sideJitter = 10;
-                break;
+                    destroyer.sideJitter = 10;
+                    break;
+
+                }
 
             //no.11 - 거울 속에 비친 나
             case 11:
-                var camCon = Camera.main.GetComponent<CameraController>();
-                camCon.isMirrored = true;
-
-                //거울 해제
-                GameObject.Find("Level").transform.Find("Mirrored").gameObject.SetActive(true);
-                GameObject.Find("StartWall").SetActive(false);
-
-                //정방향 클리어존 끄기
-                GameObject.Find("NormalClearZone").SetActive(false);
-
-                //함정 상태 동기화
-                var trapInfo = GameObject.Find("Level").transform.Find("Normal").Find("TrapInfo");
-
-                for (int i = 0; i < trapInfo.childCount; i++)
                 {
-                    trapInfo.GetChild(i).GetComponent<TrapSetter>().RandomSet();
+                    //카메라 연출 활성화
+                    var camCon = Camera.main.GetComponent<CameraController>();
+                    camCon.isMirrored = true;
+
+                    //반대쪽 활성화
+                    GameObject.Find("Level").transform.Find("Mirrored").gameObject.SetActive(true);
+                    GameObject.Find("StartWall").SetActive(false);
+
+                    //정방향 클리어존 끄기
+                    GameObject.Find("NormalClearZone").SetActive(false);
+
+                    //함정 상태 동기화
+                    var trapInfo = GameObject.Find("Level").transform.Find("Normal").Find("TrapInfo");
+
+                    for (int i = 0; i < trapInfo.childCount; i++)
+                    {
+                        trapInfo.GetChild(i).GetComponent<TrapSetter>().RandomSet();
+                    }
+
+                    //var mappinSetter = FindObjectsByType<MapPinSetter>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+                    //foreach (var com in mappinSetter)
+                    //    com.maxPinCount = 4;
+
+                    break;
                 }
 
-                var mirroredApearTrap = GameObject.Find("Level").transform.Find("Mirrored").Find("Trap").Find("ApearTrapGrid M").Find("ApearTrap M").GetComponent<ReactivePlatform>();
-                var normalApearTrap = GameObject.Find("Level").transform.Find("Normal").Find("Trap").Find("ApearTrapGrid").Find("ApearTrap").GetComponent<ReactivePlatform>();
-
-                var mappinSetter = FindObjectsByType<MapPinSetter>(FindObjectsInactive.Include,FindObjectsSortMode.None);
-
-                foreach (var com in mappinSetter)
-                    com.maxPinCount = 4;
-
-                //var mappinSetter = FindObjectOfType<MapPinSetter>();
-                //mappinSetter.maxPinCount = 4;
-
-                if (!normalApearTrap.TrapIsOff)
-                {
-                    mirroredApearTrap.type = normalApearTrap.type;
-                    mirroredApearTrap.SetPlatformOption();
-                }
-                else
-                {
-                    mirroredApearTrap.TrapOff();
-                }
-
-                break;
 
             //no.12 - 마른 하늘에 날벼락 (돌 굴러가요!!)
-            //todo - 돌넣어야 함
             case 12:
-                GameObject.Find("PlatformManager").GetComponent<PlatformManager>().SetPlatformType(PlatformType.Alter);
-                break;
-
+                {
+                    GameObject.Find("PlatformManager").GetComponent<PlatformManager>().SetPlatformType(PlatformType.Alter);
+                    GameObject.Find("Anomaly").transform.Find("BigStone").gameObject.SetActive(true);
+                    break;
+                }
             default:
                 break;
         }
