900db190dd942b189384dcb6dde72b691d2921c1
Foodie_Pudding <sacar1920@gmail.com>
2025-05-19 20:57:12 +0900

    BG패럴렉스 수정

--- Assets/Sunken/Scripts/ParallaxBG.cs
+++ Assets/Sunken/Scripts/ParallaxBG.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ParallaxBG : MonoBehaviour
 {
     Camera cam;
-    Vector2 startPos;
+    Vector3 startPos;
     float length;
 
     [Header("스크롤 속도")]
@@ -14,20 +14,13 @@ public class ParallaxBG : MonoBehaviour
     [Header("배경 오프셋")]
     public Vector2 offset = Vector2.zero;
 
-    private void OnValidate()
-    {
-        Start();
-        LateUpdate();
-    }
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-        startPos = new Vector2(cam.transform.position.x, cam.transform.position.y) + offset;
-        length = GetComponent<SpriteRenderer>().size.x * transform.lossyScale.x;
-
-        transform.position = new Vector3(startPos.x, startPos.y, transform.position.z);
+        startPos = cam.transform.position + new Vector3(offset.x, offset.y, 0);
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -35,7 +28,7 @@ public class ParallaxBG : MonoBehaviour
     {
         float distanceX = cam.transform.position.x * parallaxSpeedX;
         float distanceY = cam.transform.position.y * parallaxSpeedY;
-        float movement = cam.transform.position.x * (1f-parallaxSpeedX);
+        float movement = cam.transform.position.x * (1f - parallaxSpeedX);
 
         transform.position = new Vector3(startPos.x + distanceX, startPos.y + distanceY, transform.position.z);
 
