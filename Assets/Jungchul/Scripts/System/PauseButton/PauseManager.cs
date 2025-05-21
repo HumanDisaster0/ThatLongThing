8b532787ae187eb0356b201e811396d4017ee4a9
rock-oon <jcgs0080@gmail.com>
2025-05-22 02:32:18 +0900

    일시정지Pause 완성

--- Assets/Jungchul/Scripts/System/PauseButton/PauseManager.cs
+++ Assets/Jungchul/Scripts/System/PauseButton/PauseManager.cs
@@ -1,32 +1,23 @@
-using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
-using Unity.VisualScripting;
+using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
     public static PauseManager Instance;
 
-    public GameObject pauseOverlay;
-    public GameObject pausePanel;
+    public GameObject pauseCanvasPrefab;
+    private GameObject pauseCanvasInstance;
 
-    //public GameObject pauseButtonPrefab;
-    //public GameObject pauseButtonInstance;
-
-    //public GameObject pausePanelPrefab;
-    //private GameObject pausePanelInstance;
-
-    public CustomClickable resumBtn;
-    public CustomClickable returnToTitleBtn;
-    public CustomClickable exitBtn;
-
-    //public bool isPause = false;
+    private CustomClickable resumBtn;
+    private CustomClickable returnToTitleBtn;
+    private CustomClickable exitBtn;
 
     public enum isPause
     {
         PAUSE,
         NOTPAUSE,
     }
+
     public isPause pState;
     public isPause prePState;
 
@@ -34,134 +25,111 @@ public class PauseManager : MonoBehaviour
     {
         if (Instance != null && Instance != this)
         {
+            Debug.LogWarning("중복 PauseManager 제거됨");
             Destroy(gameObject);
             return;
         }
-
+        Debug.Log("PauseManager 생성됨");
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
-       
+        pState = isPause.NOTPAUSE;
+        prePState = isPause.NOTPAUSE;
+        Debug.Log("PauseManager Awake - 이벤트 등록");
 
         SceneManager.sceneLoaded += OnSceneLoaded;
 
-        Scene current = SceneManager.GetActiveScene();
-        OnSceneLoaded(current, LoadSceneMode.Single);
-
-        pState = isPause.NOTPAUSE;
-        prePState = isPause.NOTPAUSE;
-
-
-    }
-
-    private void Start()
-    {
-        //if (resumBtn != null)
-        //    resumBtn.SetClickAction(() => Resume());
-
-        //if (returnToTitleBtn != null)
-        //    returnToTitleBtn.SetClickAction(() => ReturnToTitle());
-
-        //if (exitBtn != null)
-        //    exitBtn.SetClickAction(() => ExitGame());
     }
 
     private void Update()
     {
-        if (resumBtn == null)
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name != "Title")
         {
-            Debug.Log("리섬버튼 안달렸어");
-
-        }
-        if (Input.GetKeyDown(KeyCode.Escape) && !(SceneManager.GetActiveScene().name == "Title"))// || SceneManager.GetActiveScene().name == "Prologue") )
-        {
-            if (pState == isPause.NOTPAUSE)
-            {
-                pState = isPause.PAUSE;
-            }
-            else
-            {
-                pState = isPause.NOTPAUSE;
-            }
+            pState = (pState == isPause.NOTPAUSE) ? isPause.PAUSE : isPause.NOTPAUSE;
         }
 
         if (prePState != pState)
         {
             prePState = pState;
-            switch (pState)
-            {
-                case isPause.PAUSE:
-                    Pause();
-                    
-                    break;
-
-                case isPause.NOTPAUSE:
-                    Resume();
-
-                    break;
-            }
+            if (pState == isPause.PAUSE)
+                Pause();
+            else
+                Resume();
         }
-
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        pausePanel = GameObject.Find("PausePanel");
-        pauseOverlay = GameObject.Find("PauseOverlay");
-
-        // 버튼을 다시 찾아서 연결
-        if (pausePanel != null)
+        if (pauseCanvasInstance != null)
         {
-            var btns = pausePanel.GetComponentsInChildren<CustomClickable>(true);
-            foreach (var btn in btns)
+            Camera mainCam = Camera.main;
+            Canvas canvas = pauseCanvasInstance.GetComponent<Canvas>();
+            if (canvas != null && mainCam != null)
             {
-                if (btn.name.Contains("Resume"))
-                {
-                    resumBtn = btn;
-                    resumBtn.SetClickAction(() => Resume());
-                }
-                else if (btn.name.Contains("Title"))
-                {
-                    returnToTitleBtn = btn;
-                    returnToTitleBtn.SetClickAction(() => ReturnToTitle());
-                }
-                else if (btn.name.Contains("Exit"))
-                {
-                    exitBtn = btn;
-                    exitBtn.SetClickAction(() => ExitGame());
-                }
+                canvas.worldCamera = mainCam;
+                Debug.Log($"[PauseManager] Canvas 카메라 설정됨: {mainCam.name}");
             }
-
-            pausePanel.SetActive(false);
+            else
+            {
+                Debug.LogWarning("[PauseManager] Canvas나 Main Camera를 찾지 못했습니다.");
+            }
         }
 
-        if (pauseOverlay != null)
-            pauseOverlay.SetActive(false);
-
-        pausePanel?.SetActive(false);
-        pauseOverlay?.SetActive(false);
+        // 2. 일시정지 해제 처리
+        
+        Resume();
     }
 
+    private void CreatePauseUI()
+    {
+        if (pauseCanvasInstance != null) return;
+
+        pauseCanvasInstance = Instantiate(pauseCanvasPrefab);
+        DontDestroyOnLoad(pauseCanvasInstance);
+
+        var btns = pauseCanvasInstance.GetComponentsInChildren<CustomClickable>(true);
+        foreach (var btn in btns)
+        {
+            if (btn.name.Contains("Resume"))
+            {
+                resumBtn = btn;
+                resumBtn.SetClickAction(() => Resume());
+            }
+            else if (btn.name.Contains("Title"))
+            {
+                returnToTitleBtn = btn;
+                returnToTitleBtn.SetClickAction(() => ReturnToTitle());
+            }
+            else if (btn.name.Contains("Exit"))
+            {
+                exitBtn = btn;
+                exitBtn.SetClickAction(() => ExitGame());
+            }
+        }
+    }
 
     public void Pause()
     {
+        if (pauseCanvasInstance == null)
+            CreatePauseUI();
+
         Time.timeScale = 0f;
-        pauseOverlay?.SetActive(true);
-        pausePanel?.SetActive(true);
+        pauseCanvasInstance.SetActive(true);
     }
 
     public void Resume()
     {
         Time.timeScale = 1f;
-        pauseOverlay?.SetActive(false);
-        pausePanel?.SetActive(false);
+        if (pauseCanvasInstance != null)
+            pauseCanvasInstance.SetActive(false);
     }
 
     public void ReturnToTitle()
     {
-        print("타이틀로돌아감요!");
-        Time.timeScale = 1f;
-        NonePlaySceneManager.Instance.SetSceneState(NonePlaySceneManager.npSceneState.TITLE);
+        Resume();
+        Destroy(pauseCanvasInstance);
+        pauseCanvasInstance = null;
+
         SceneManager.LoadScene("Title");
     }
 
@@ -174,28 +142,17 @@ public class PauseManager : MonoBehaviour
 #endif
     }
 
-    //private void CreatePausePanel()
-    //{
-    //    if (pausePanelPrefab == null)
-    //    {
-    //        Debug.LogError("PausePanelPrefab이 설정되지 않았습니다.");
-    //        return;
-    //    }
-
-    //    pausePanelInstance = Instantiate(pausePanelPrefab);
-    //    pausePanelInstance.name = "PausePanel";
-    //    pausePanelInstance.SetActive(false);
-
-    //    // 자식 버튼들 연결
-    //    var btns = pausePanelInstance.GetComponentsInChildren<CustomClickable>(true);
-    //    foreach (var btn in btns)
-    //    {
-    //        if (btn.name.Contains("Resume"))
-    //            btn.SetClickAction(() => Resume());
-    //        else if (btn.name.Contains("Title"))
-    //            btn.SetClickAction(() => ReturnToTitle());
-    //        else if (btn.name.Contains("Exit"))
-    //            btn.SetClickAction(() => ExitGame());
-    //    }
-    //}
+    private void LateUpdate()
+    {
+        if (pauseCanvasInstance != null)
+        {
+            Canvas canvas = pauseCanvasInstance.GetComponent<Canvas>();
+            if (canvas != null && canvas.worldCamera == null)
+            {
+                canvas.worldCamera = Camera.main;
+                if (canvas.worldCamera != null)
+                    Debug.Log("[PauseManager] LateUpdate에서 카메라 연결 완료");
+            }
+        }
+    }
 }
