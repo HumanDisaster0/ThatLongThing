05012d37cf2278de268eda8748605cbf196e1c8c
minkee009 <minkee5004@gmail.com>
2025-05-22 17:59:38 +0900

    씬 변경사항 적용

--- Assets/Minki/Scripts/Portal/RedPortalInteraction.cs
+++ Assets/Minki/Scripts/Portal/RedPortalInteraction.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class RedPortalInteraction : MonoBehaviour
 {
+    public Transform popup;
+
+    BoxCollider2D m_col;
+
+    private void Start()
+    {
+        m_col = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            popup.gameObject.SetActive(true);
+        }    
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            popup.gameObject.SetActive(false);
+        }
+    }
+
     public void EnterPortal()
     {
         GoldManager.Instance.ejectionCount++;
