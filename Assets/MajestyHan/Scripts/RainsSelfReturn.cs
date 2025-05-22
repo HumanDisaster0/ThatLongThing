bcbd7d9070e68880376e5d6e798eca901a63c27e
Foodie_Pudding <sacar1920@gmail.com>
2025-05-22 11:41:22 +0900

    골드레인 먹으면 골드+1

--- Assets/MajestyHan/Scripts/RainsSelfReturn.cs
+++ Assets/MajestyHan/Scripts/RainsSelfReturn.cs
@@ -13,6 +13,7 @@ public class RainsSelfReturn : MonoBehaviour
         if (((1 << other.gameObject.layer) & playerLayer) != 0)
         {
             SoundManager.instance.PlayNewSound("Gold_Collect", GameObject.FindWithTag("Player"));
+            GoldManager.Instance.totalGold++;
             rainPool?.Return(gameObject);
         }
     }
