388184119fd54f668e3b8ba2ba381e32100eff34
Dareuta <jk99333@gmail.com>
2025-06-16 16:58:44 +0900

    일단 올려(3스테이지 작업 - 엔딩 진입지점 수정중)

--- Assets/MajestyHan/Scripts/EndingEnterTrigger.cs
+++ Assets/MajestyHan/Scripts/EndingEnterTrigger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class EndingEnterTrigger : MonoBehaviour
 {
@@ -12,17 +13,57 @@ public class EndingEnterTrigger : MonoBehaviour
     {
         if (collision.CompareTag("Player") || collision.CompareTag("NPC"))
         {
-            if (cam != null)
-                cam.ShakeCamera(16f, 0.5f, 2f); //사운드(효과음도 넣어주면 좋을듯) 용수철 튕기는 요상한 마법소리 - 벽에 막히는 소리
-
-            var volume = FindObjectOfType<Volume>();
-            volume.profile.TryGet(out ChromaticAberration chromatic);
-            chromatic.intensity.value = 1.0f;
-
-            GameObject.Find("Player").transform.Find("ShadowMask").gameObject.SetActive(false);
-
             var pl = collision.GetComponent<PlayerController>();
-            pl.SkipInput = true;
+            pl.SkipInput = true; // 움직임 잠금 + 맵 잠금 필요함!@#$!@#$
+
+            DirectingEnterToEnding(); //연출
+
+            SceneManager.LoadScene("EndingScene");
         }
+    }
+
+    //==============================================================================
+    IEnumerator ExpandMask(SpriteRenderer sr, float targetRadius, float duration)
+    {
+        MaterialPropertyBlock mpb = new();
+        sr.GetPropertyBlock(mpb);
+        float start = mpb.GetFloat("_MaskRadius");
+        float t = 0;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            mpb.SetFloat("_MaskRadius", Mathf.Lerp(start, targetRadius, t / duration));
+            sr.SetPropertyBlock(mpb);
+            yield return null;
+        }
+    }
+
+    //==============================================================================
+    void SetMaskColor(SpriteRenderer sr, Color color)
+    {
+        var mpb = new MaterialPropertyBlock();
+        sr.GetPropertyBlock(mpb);
+        mpb.SetColor("_MaskColor", color);
+        sr.SetPropertyBlock(mpb);
+    }
+
+    //==============================================================================
+
+    void DirectingEnterToEnding() //연출
+    {
+        if (cam != null)
+            cam.ShakeCamera(16f, 0.5f, 2f); //사운드(효과음도 넣어주면 좋을듯) 용수철 튕기는 요상한 마법소리 - 벽에 막히는 소리
+
+        var volume = FindObjectOfType<Volume>();
+        volume.profile.TryGet(out ChromaticAberration chromatic);
+        chromatic.intensity.value = 1.0f; // 색수차
+
+        var maskObj = GameObject.Find("Player").transform.Find("ShadowMask");
+        var sr = maskObj.GetComponent<SpriteRenderer>();
+
+        SetMaskColor(sr, Color.magenta); // 마스크 기본색 변경
+        StartCoroutine(ExpandMask(sr, 0.3f, 1.5f)); // 마스크 크기 변경
+                                                    //maskObj.gameObject.SetActive(false); // 이건 확장 끝난 후에 하고 싶으면 코루틴 마지막에 옮겨도 됨
     }
 }
