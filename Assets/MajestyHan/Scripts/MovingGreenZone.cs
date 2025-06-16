387f82c56298aedf339bfbe2c1fc42b59a2b3a44
Dareuta <jk99333@gmail.com>
2025-06-16 10:59:02 +0900

    거울 스테이지 수정 / 효과음 하나 넣어주면 좋을듯

--- Assets/MajestyHan/Scripts/MovingGreenZone.cs
+++ Assets/MajestyHan/Scripts/MovingGreenZone.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.Examples;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 
+
+
+
 public class MovingGreenZone : MonoBehaviour
 {
     public string spawnPointName = "PlayerSpawnPoint";
+
+    public CameraController cam;
+
     private int count;
     private void Start()
     {
         count = 0;
-        transform.position = new Vector3(27f, transform.position.y, transform.position.z); //x 27로 고정
+        transform.position = new Vector3(27f, transform.position.y, transform.position.z); //x 27로 고정        
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("NPC"))
         {
+            if (cam != null)
+                cam.ShakeCamera(16f, 0.5f, 2f); //사운드(효과음도 넣어주면 좋을듯) 용수철 튕기는 요상한 마법소리 - 벽에 막히는 소리
+
             var spawn = collision.GetComponent<PlayerSpawnManager>();
             if (spawn != null)
             {
