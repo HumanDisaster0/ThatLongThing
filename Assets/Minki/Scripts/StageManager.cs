fa89807845a3a0e6e62116500670a14d49ed7393
Dareuta <jk99333@gmail.com>
2025-06-16 11:04:06 +0900

    거울 스테이지 수정(포탈 보이게 변경)

--- Assets/Minki/Scripts/StageManager.cs
+++ Assets/Minki/Scripts/StageManager.cs
@@ -397,7 +397,7 @@ public class StageManager : MonoBehaviour
                     GameObject.Find("Level").transform.Find("Mirrored").gameObject.SetActive(true);
                     GameObject.Find("StartWall").SetActive(false);                    
                     //정방향 클리어존 끄기
-                    GameObject.Find("NormalClearZone").SetActive(false);
+                    //GameObject.Find("NormalClearZone").SetActive(false);
                     GameObject.Find("DeathTrigger").transform.Find("GreenZoneZone").gameObject.SetActive(true);
 
                     //함정 상태 동기화
