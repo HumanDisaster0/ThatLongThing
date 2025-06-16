4a1d1e8421b0c2e2369afb431485f656a230815e
Foodie_Pudding <sacar1920@gmail.com>
2025-06-16 19:58:22 +0900

    버그수정
    크아앙ㄱ

--- Assets/Jungchul/Scripts/System/PauseButton/PauseManager.cs
+++ Assets/Jungchul/Scripts/System/PauseButton/PauseManager.cs
@@ -111,12 +111,12 @@ public class PauseManager : MonoBehaviour
 
     public void Pause()
     {
-        MapOnOffControl mc = GameObject.FindAnyObjectByType<MapOnOffControl>();
-        if (mc != null)
-        {
-            mc.activeControl = false;
-            mc.AddUIStack(this.gameObject);
-        }
+        //MapOnOffControl mc = GameObject.FindAnyObjectByType<MapOnOffControl>();
+        //if (mc != null)
+        //{
+        //    mc.activeControl = false;
+        //    mc.AddUIStack(this.gameObject);
+        //}
             
 
         if (pauseCanvasInstance == null)
@@ -128,12 +128,12 @@ public class PauseManager : MonoBehaviour
 
     public void Resume()
     {
-        MapOnOffControl mc = GameObject.FindAnyObjectByType<MapOnOffControl>();
-        if (mc != null)
-        {
-            mc.activeControl = true;
-            mc.AddUIStack(this.gameObject);
-        }
+        //MapOnOffControl mc = GameObject.FindAnyObjectByType<MapOnOffControl>();
+        //if (mc != null)
+        //{
+        //    mc.activeControl = true;
+        //    mc.AddUIStack(this.gameObject);
+        //}
 
         SoundManager.instance?.GetComponent<SettingCanvasController>().SetSettingCanvas(false);
         Time.timeScale = 1f;
