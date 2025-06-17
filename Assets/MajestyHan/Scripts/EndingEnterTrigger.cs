c341ee88e0645082b91cf4a0c43602aa8459dcfd
Dareuta <jk99333@gmail.com>
2025-06-17 14:35:11 +0900

    연출(효과음 필요함)
    "해줘"

--- Assets/MajestyHan/Scripts/EndingEnterTrigger.cs
+++ Assets/MajestyHan/Scripts/EndingEnterTrigger.cs
@@ -16,6 +16,9 @@ public class EndingEnterTrigger : MonoBehaviour
             var pl = collision.GetComponent<PlayerController>();
             pl.SkipInput = true; // 움직임 잠금 + 맵 잠금 필요함!@#$!@#$
 
+            var map = FindFirstObjectByType<MapOnOffControl>();
+            map.activeControl = false; // 맵 끄기
+
             StartCoroutine(EnterToEndingSequence());
         }
     }
